e4f69d67b1ce5cd4dd22f383525bd7889d832b5f
Elijah2005 <league@WTS22.attlocal.net>
2018-04-05 18:09:48 -0700

    part 11 step 9

--- src/Alien.java
+++ src/Alien.java
@@ -2,14 +2,15 @@ import java.awt.Color;
 import java.awt.Graphics;
 
 public class Alien extends GameObject {
-Alien(int x, int y, int width, int height){
-	super(x, y, width, height);
-}
-void update(){
-	
-}
-void draw(Graphics g){
-	g.setColor(Color.YELLOW);
-	g.fillRect(x, y, width, height);
-}
+	Alien(int x, int y, int width, int height) {
+		super(x, y, width, height);
+	}
+
+	void update() {
+	}
+
+	void draw(Graphics g) {
+		g.setColor(Color.YELLOW);
+		g.fillRect(x, y, width, height);
+	}
 }
--- src/ObjectManager.java
+++ src/ObjectManager.java
@@ -5,6 +5,7 @@ public class ObjectManager {
 	Paddle paddle;
 	Ball ball;
 	Paddle paddle2;
+	ArrayList<Alien> alien = new ArrayList<Alien>();
 
 	ObjectManager(Paddle paddle, Ball ball, Paddle paddle2) {
 		this.paddle = paddle;
@@ -16,13 +17,20 @@ public class ObjectManager {
 		paddle.update();
 		ball.update();
 		paddle2.update();
+
 	}
 
 	void draw(Graphics g) {
 		paddle.draw(g);
 		ball.draw(g);
 		paddle2.draw(g);
-
+		for (int i = 0; i < alien.size(); i++) {
+			Alien a = alien.get(i);
+			a.draw(g);
+		}
 	}
 
+	void addAlien() {
+
+	}
 }
